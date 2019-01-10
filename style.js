611987222ca6c04c87148a5499045a1705658524
Grizzly <grizzlyxyz@gmail>
2019-01-10 14:58:11 +0100

    exo5

---
+++ style.js
@@ -0,0 +1,4 @@
+// Exo 05 - Après avoir effectué une petite recherche de functions sur MDN "Mozilla" effacer l'espace au début de la variable "sentence01", les espaces autour de la variable "sentence02" et l'espace de la fin de "sentence03" pour former la phrase suivante : "C'est un petit pas pour l'Homme mais un grand pas pour l'humanité." et stocker le résultant dans une variable du nom de bestSentence
+let sentence01 = " C'est un petit pas ";
+let sentence02 = " Pour l'Homme ";
+let sentence03 = " Mais un grand pas pour l'humanité. ";
